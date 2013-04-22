b3860c9bbf760aad7a0a83cac43e44f0f7427d8d
Jeff Behnke <jeff@valid-webs.com>
2013-04-22 18:02:55 -0500

    Moving some things around. Need to write new instructions for each software

--- WordPress/wpdb_docs.php
+++ WordPress/wpdb_docs.php
@@ -1,20 +1,9 @@
 <?php 
-/* Use an autoloader on include to load this but do not instance it 
- * OPTION ONE (autoloaded or included)
- * Name the class like this
- * class wpdb_docs extends wpdb {}
- *
- * /** @var $tag wpdb_docs */
- * $tag_urls = get_tag_link($tag->term_id);
- *
- * OPTION TWO and probably the best
- * Alternatively you can just have it class wpdb {} and put it in the project root and phpStorm will find it
- * This way it does not need to be autoloaded or inlcuded
- *
- * /** @var $tag wpdb */
- * $tag_urls = get_tag_link($tag->term_id);
- *
- */
+
+// Examples of inline referencing
+
+/** @var $tag wpdb */
+// $tag_urls = get_tag_link($tag->term_id);
 
 /**
  * @property wpdb  $ID
